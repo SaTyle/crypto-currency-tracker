2adbb122e88ee14b82f6cd5742bf7fd9616b705c
SaTyle <zxcvwifi@gmail.com>
2023-05-18 17:27:56 +0530

    Crypto Analyzer wenddeD

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter
+            Crypto Analyzer
           </Typography>
           <Typography
             variant="subtitle2"
