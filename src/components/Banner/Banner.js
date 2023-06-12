847c098af5e3a7b5c12092aab33b423089294998
SaTyle <zxcvwifi@gmail.com>
2023-06-12 11:48:55 +0530

    a

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -51,7 +51,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Get all the Info regarding your favorite Crypto Currency
+            Yash mC
           </Typography>
         </div>
         <Carousel />
